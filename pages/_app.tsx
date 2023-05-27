18fd411a997d13bb381e8b97bfe0e8f2fbba727d
joshoguh <joshuaoguh12@gmail.com>
2023-05-27 05:01:29 -0400

    new redesign fix2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <div>
+          <Head>
+              <style>
+                  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap');
+              </style>
+              <title>Joshua Oguh</title>
+          </Head>
           <Component {...pageProps} />
       </div>
   )
